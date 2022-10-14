f779069f015594328b67a451360d0b78f9404726
BrunoSouza <desouza88@hotmail.com>
2022-10-14 17:09:23 -0300

    Consertos Lint CSS e JS

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let email = document.getElementById('email')
-let senha = document.getElementById('password')
-let button = document.getElementById('send')
+const email = document.getElementById('email');
+const senha = document.getElementById('password');
+const button = document.getElementById('send');
 
-button.addEventListener('click', function(){
-    if(email.value == 'tryber@teste.com' & senha.value == '123456') {
-        window.alert("Olá, Tryber!")
-    } else {
-        window.alert('Email ou senha inválidos.')
-    }
-})
+button.addEventListener('click', () => {
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos.');
+  }
+});
